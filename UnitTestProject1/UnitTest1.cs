716c2d28f460e59cea1f0441f297346d13331dc2
234970 <234970@KB37-317-C11>
2026-05-18 10:10:15 +0300

    метод инициализации перед каждым тестом

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -8,10 +8,16 @@ namespace ttt.Tests
     [TestClass]
     public class UnitTest1
     {
-        private const int TestP = 17;   
-        private const int TestQ = 19;   
-        private BigInteger _publicKey;   
-        private BigInteger _privateKey;  
-        private BigInteger _modulus;     
+        private const int TestP = 17;
+        private const int TestQ = 19;
+        private BigInteger _publicKey;
+        private BigInteger _privateKey;
+        private BigInteger _modulus;
+
+        [TestInitialize]
+        public void Init()
+        {
+            RsaAlgorithm.GenerateKeys(TestP, TestQ, out _publicKey, out _privateKey, out _modulus);
+        }
     }
 }

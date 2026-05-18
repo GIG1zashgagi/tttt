0bd78e30d00770d0076226548de9aae12f1d031b
234970 <234970@KB37-317-C11>
2026-05-18 09:26:38 +0300

    создаем генератор случайных чисел для создания простых чисел

--- ttt/RsaAlgorithm.cs
+++ ttt/RsaAlgorithm.cs
@@ -5,5 +5,24 @@ using System.Collections.Generic;
 
 namespace ttt
 {
+    public class RsaAlgorithm
+    {
+        private static Random _random = new Random();
 
+        public static bool IsPrime(int number)
+        {
+            if (number < 2) return false;
+
+            if (number == 2) return true;
+
+            if (number % 2 == 0) return false;
+
+            int limit = (int)Math.Sqrt(number);
+            for (int i = 3; i <= limit; i += 2)
+            {
+                if (number % i == 0) return false;  
+            }
+            return true;  
+        }
+    }
 }
